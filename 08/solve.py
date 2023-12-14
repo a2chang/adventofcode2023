8b9a007dddcbf6bb63771a02fb6ed8b7470339eb
Andre Chang <a2chang@frenzy.local>
2023-12-13 22:34:12 -0800

    Day 8 done

--- 08/solve.py
+++ 08/solve.py
@@ -34,37 +34,85 @@ def parse(lines):
 	return locs, net, commands
 
 
-def get_wheres1(_):
+def get_starts1(_):
 	return [ 'AAA' ]
 
+def done1(where):
+	return where == 'ZZZ'
 
-def done1(wheres):
-	return wheres[0] == 'ZZZ'
+
+def get_starts2(locs):
+	return [ l for l in locs if l[2] == 'A' ]
+
+def done2(where):
+	return where[2] == 'Z'
 
 
-def traverse(wheres, net, commands, endcond):
+def traverse(start, net, commands, endcond):
 	i = 0
+	where = start
 	steps = 0
-	while not endcond(wheres):
+	while not endcond(where):
 		steps = steps + 1
 		c = commands[i]
 		i = i + 1
 		if i == len(commands):
 			i = 0
 
-		wheres = [ net.get('%s:%s' % (w, c)) for w in wheres ]
+		#print where
+		where = net.get('%s:%s' % (where, c))
 	return steps
 
 
+def traverse_cycle(start, net, commands, endcond):
+	i = 0
+	mod = 0
+	where = start
+	end = []
+	visited = {''}
+	while not ((mod == 0) and where in visited):
+		if mod == 0:
+			visited.add(where)
+		c = commands[mod]
+		where = net.get('%s:%s' % (where, c))
+		i = i + 1
+		mod = i % len(commands)
+		if done2(where):
+			end.append(i)
+	return end
+
+
+def check_end(n, ends):
+	for e in ends:
+		if ((n-1) % e[-1]) + 1 not in e:
+			return False
+	return True
+
 def main():
 	lines = read(test_input)
+	locs, net, commands = parse(lines)
 
 	# Part 1
-	locs, net, commands = parse(lines)
-	print traverse(get_wheres1(locs), net, commands, done1)
+	print traverse(get_starts1(locs)[0], net, commands, done1)
 
 	# Part 2
+	ends = []
+	for s in get_starts2(locs):
+		end = traverse_cycle(s, net, commands, done2)
+		#print end
+		ends.append(end)
+	l = len(commands)
+	n = 1
+	for e in ends:
+		n = n * e[0] / l
+	print n * l
 
+	return
+	while True:
+		n = n + 1
+		if check_end(n, ends):
+			print n
+			break
 
 if __name__ == "__main__":
 	main()
