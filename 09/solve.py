65c16ad33c2c647c099f20b912b7e41537264bc7
Andre Chang <a2chang@frenzy.local>
2023-12-13 23:32:38 -0800

    Day 9 done

--- 09/solve.py
+++ 09/solve.py
@@ -19,38 +19,40 @@ def parse_line(line):
 	vals = [ eval(v) for v in line.split() ]
 	n = len(vals)
 	pows = []
-	for i in range(n+1):
+	for i in range(-1, n+1):
 		pows.append( [ i**j for j in range(n) ] )
 	#print pows
 	#print vals
-	arr_pows = numpy.array(pows[:-1])
+	arr_pows = numpy.array(pows[1:-1])
 	arr_vals = numpy.array(vals)
 
 	coeffs = numpy.linalg.solve(arr_pows, arr_vals)
 	#print coeffs
 	arr_pows = numpy.array(pows[-1])
 	n = numpy.dot(coeffs, arr_pows)
+	arr_pows = numpy.array(pows[0])
+	p = numpy.dot(coeffs, arr_pows)
 	#print n
-	return n
+	return p, n
 	
 
 
 def parse(lines):
-	sum = 0
+	sum1 = 0
+	sum2 = 0
 	for line in lines:
 		line = line.strip('\n')
-		sum = sum + parse_line(line)
-	print sum
+		p, n = parse_line(line)
+		sum1 = sum1 + round(n)
+		sum2 = sum2 + round(p)
+	print sum1
+	print sum2
 
 
 def main():
 	lines = read(test_input)
 
-	# Part 1
 	parse(lines)
-
-	# Part 2
-	#parse(lines, True)
 
 
 if __name__ == "__main__":
