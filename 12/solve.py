62b0b6d456cba3f09b6b0fbbd079cc5fc6282abc
Andre Chang <a2chang@frenzy.local>
2023-12-18 09:02:53 -0800

    Day 12 done

--- 12/solve.py
+++ 12/solve.py
@@ -70,29 +70,62 @@ def iterate(pattern, wcs, signature):
 	return matches
 
 
+def gobble(pat, sig, searched):
+	#print ('gobble %s %s' % (pat, sig))
+	pat = pat.lstrip('.')
+
+	cache_sigs = searched.get(pat, None)
+	if cache_sigs is not None:
+		count = cache_sigs.get(str(sig), None)
+		if count is not None:
+			return count
+
+	# End of our groups
+	if sig == []:
+		#print ('end sig')
+		if pat.count('#') == 0:
+			return 1
+		else:
+			return 0
+
+	# Are there enough characters left for the groups?
+	if len(pat) < (sum(sig) + len(sig) - 1):
+		#print ('too short')
+		return 0
+	need = sig[0]
+	if len(pat) < need:
+		return 0
+
+	count = 0
+	if pat[0:need].count('.') == 0:
+		if pat[need] != '#':
+			count = count + gobble(pat[need+1:], sig[1:], searched)
+
+	if pat[0] != '#':
+		count = count + gobble(pat[1:], sig, searched)
+
+	if cache_sigs is None:
+		cache_sigs = {}
+		searched[pat] = cache_sigs
+	cache_sigs[str(sig)] = count
+
+	return count
+
+
 def parse(line, fudge = 1):
 	pattern, s = line.split()
+
+	pattern = '?'.join( [ pattern ] * fudge )
 	signature = s.split(',')
 	signature = [ eval(s) for s in signature ] * fudge
 	#print pattern
 	#print signature
 
-	# Find wildcards
-	pattern = pattern * fudge
-	wcs = get_wcs(pattern)
-
-	# Iterate through all possible wildcard combinations
-	#print pattern
-	#print wcs
-	#print signature
-	return iterate(pattern, wcs, signature)
+	return gobble(pattern + '.', signature, {})
 
 
 def main():
 	lines = read(test_input)
-
-	#maze.test()
-	#return
 
 	# Part 1
 	sum = 0
@@ -103,10 +136,7 @@ def main():
 	# Part 2
 	sum = 0
 	for line in lines:
-		#n = parse(line, 5)
-		n = 0
-		#print n
-		sum = sum + n
+		sum = sum + parse(line, 5)
 	print sum
 
 
