e6e507988cbde5f01f024aaa4207a82ee86d97e3
Andre Chang <a2chang@frenzy.local>
2024-01-04 13:28:43 -0800

    Day 19 done

--- 19/solve.py
+++ 19/solve.py
@@ -7,6 +7,7 @@ test_input = 'adventofcode.com_2023_day_19_input.txt'
 
 class State():
 	start = 'in'
+	breaks = {}
 
 	def __init__(self):
 		# Character of node
@@ -25,6 +26,14 @@ class State():
 	def is_bad(state):
 		return (state == 'R')
 
+	@staticmethod
+	def add_break(var, comp, value):
+		if comp == '<':
+			value = value - 1
+		if State.breaks.get(var, None) is None:
+			State.breaks[var] = []
+		State.breaks[var].append(value)
+
 	def _parse_transition(self, t):
 		if t.find(':') == -1:
 			return [ None, None, None, t ]
@@ -33,6 +42,7 @@ class State():
 		var = cond[0]
 		comp = cond[1]
 		value = eval(cond[2:])
+		State.add_break(var, comp, value)
 		return [ var, comp, value, state ]
 
 	def parse(self, rule):
@@ -61,8 +71,25 @@ class State():
 		print self.name
 		print self._transitions
 
+	def get_rule(self, i):
+		return self._transitions[i]
+
+	@staticmethod
+	def sort():
+		for k in State.breaks.keys():
+			State.breaks[k].append(Part.var_max)
+			State.breaks[k].sort()
+
+	@staticmethod
+	def debug_breaks():
+		for k in State.breaks.keys():
+			print '%s:%d:%s' % (k, len(State.breaks[k]), State.breaks[k])
+
 
 class Part():
+	var_min = 1
+	var_max = 4000
+
 	def __init__(self):
 		self.vars = {}
 
@@ -102,6 +129,7 @@ class Machine():
 				part.parse(line)
 				#part.debug()
 				self._parts.append(part)
+		State.sort()
 
 	def run_part(self, part):
 		state = State.start
@@ -120,6 +148,69 @@ class Machine():
 		print total
 
 
+	def _traverse(self, state, rule_num, bounds_str):
+		if State.is_bad(state):
+			return 0
+
+		if State.is_good(state):
+			count = 1
+			bounds = eval(bounds_str)
+			for v in bounds.values():
+				count = count * (v[1] - v[0] + 1)
+			return count
+
+		node = self._states.get(state, None)
+		rule = node.get_rule(rule_num)
+
+		if rule[1] is None:
+			# traverse to next state
+			count = self._traverse(rule[3], 0, bounds_str)
+			return count
+		elif rule[1] == '<':
+			count = 0
+
+			bounds = eval(bounds_str)
+			if bounds[rule[0]][1] >= rule[2]:
+				bounds[rule[0]][1] = rule[2] - 1
+			if bounds[rule[0]][1] >= bounds[rule[0]][0]:
+				count = count + self._traverse(rule[3], 0, str(bounds))
+
+			bounds = eval(bounds_str)
+			if bounds[rule[0]][0] < rule[2]:
+				bounds[rule[0]][0] = rule[2]
+			if bounds[rule[0]][1] >= bounds[rule[0]][0]:
+				count = count + self._traverse(state, rule_num+1, str(bounds))
+
+			return count
+		elif rule[1] == '>':
+			count = 0
+
+			bounds = eval(bounds_str)
+			if bounds[rule[0]][0] <= rule[2]:
+				bounds[rule[0]][0] = rule[2] + 1
+			if bounds[rule[0]][1] >= bounds[rule[0]][0]:
+				count = count + self._traverse(rule[3], 0, str(bounds))
+
+			bounds = eval(bounds_str)
+			if bounds[rule[0]][1] > rule[2]:
+				bounds[rule[0]][1] = rule[2]
+			if bounds[rule[0]][1] >= bounds[rule[0]][0]:
+				count = count + self._traverse(state, rule_num+1, str(bounds))
+
+			return count
+
+
+	def find_accepts(self):
+		bounds = {
+			'x': [ Part.var_min, Part.var_max ],
+			'm': [ Part.var_min, Part.var_max ],
+			'a': [ Part.var_min, Part.var_max ],
+			's': [ Part.var_min, Part.var_max ],
+		}
+		count = self._traverse(State.start, 0, str(bounds))
+		print count
+
+
 	def test(self):
 		print self._states
 		print self._parts
@@ -136,8 +227,10 @@ def main():
 	lines = read(test_input)
 	m = Machine()
 	m.load(lines)
+	#State.debug_breaks()
 	m.run_parts()
 	#m.test()
+	m.find_accepts()
 
 
 if __name__ == "__main__":
